55e3619af6c87e8930459a770c59c66847028568
aakarshan <aakartionsimkhada@gmail.com>
2024-08-22 20:18:57 -0700

    Share window pops up with Input to provide email address to share document

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -13,6 +13,9 @@ import React, { useState } from "react";
 import { useSelf } from "@liveblocks/react/suspense";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import UserTypeSelector from "./UserTypeSelector";
 
 const ShareModal = ({
   roomId,
@@ -49,6 +52,21 @@ const ShareModal = ({
             Select which users can view and edit this document
           </DialogDescription>
         </DialogHeader>
+        <Label htmlFor="email" className="mt-6 text-blue-100">
+          Email address
+        </Label>
+        <div className="flex items-center gap-3">
+          <div className="flex flex-1 rounded-md bg-dark-400">
+            <Input
+              id="email"
+              placeholder="Enter email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="share-input"
+            />
+            <UserTypeSelector userType={userType} setUserType={setUserType} />
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );

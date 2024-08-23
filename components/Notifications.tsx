0bb97a8dab3af123f3c58335d4c828c78ff1a205
aakarshan <aakartionsimkhada@gmail.com>
2024-08-22 21:55:33 -0700

    Install Shadcn Popover and added default code

---
+++ components/Notifications.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+const Notifications = () => {
+  return (
+    <Popover>
+      <PopoverTrigger>Open</PopoverTrigger>
+      <PopoverContent>Place content for the popover here.</PopoverContent>
+    </Popover>
+  );
+};
+
+export default Notifications;

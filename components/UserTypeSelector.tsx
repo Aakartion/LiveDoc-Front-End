2c9aceb767a04b418bf8622b99df9e858c639f7b
aakarshan <aakartionsimkhada@gmail.com>
2024-08-22 20:20:59 -0700

    Shadcn Select is installed and default code is added

--- components/UserTypeSelector.tsx
+++ components/UserTypeSelector.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 const UserTypeSelector = ({
   userType,
   setUserType,
   onClickHandler,
 }: UserTypeSelectorParams) => {
-  return <div>UserTypeSelector</div>;
+  return (
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Theme" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="light">Light</SelectItem>
+        <SelectItem value="dark">Dark</SelectItem>
+        <SelectItem value="system">System</SelectItem>
+      </SelectContent>
+    </Select>
+  );
 };
 
 export default UserTypeSelector;

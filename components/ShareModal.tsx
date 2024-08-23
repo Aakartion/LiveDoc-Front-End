3071e1875f5f0f2cc8f3d1906ba3b1dc7ba9d401
aakarshan <aakartionsimkhada@gmail.com>
2024-08-22 20:07:00 -0700

    Button is added on to share with its title and description when share window pops up

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -11,6 +11,8 @@ import {
 
 import React, { useState } from "react";
 import { useSelf } from "@liveblocks/react/suspense";
+import { Button } from "./ui/button";
+import Image from "next/image";
 
 const ShareModal = ({
   roomId,
@@ -24,28 +26,32 @@ const ShareModal = ({
   const [email, setEmail] = useState("");
   const [userType, setUserType] = useState<UserType>("viewer");
   return (
-    <Dialog>
-      <DialogTrigger>Open</DialogTrigger>
-      <DialogContent>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger>
+        <Button
+          className="gradient-blue flex h-9 gap-1 px-4"
+          disabled={currentUserType !== "editor"}
+        >
+          <Image
+            src="/assets/icons/share.svg"
+            alt="share"
+            width={20}
+            height={20}
+            className="min-w-4 md:size-5"
+          />
+          <p className="mr-1 hidden sm:block">Share</p>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="shad-dialog">
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogTitle>Manage who can view this project</DialogTitle>
           <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            Select which users can view and edit this document
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
     </Dialog>
   );
 };
-const shareDocumentHandler = async () => {
-  // setLoading(true);
-  // await updateDocumentAccess({
-  //   roomId,
-  //   email,
-  //   userType: userType as UserType,
-  //   updatedBy: user.info,
-  // });
-  // setLoading(false);
-};
+const shareDocumentHandler = async () => {};
 export default ShareModal;

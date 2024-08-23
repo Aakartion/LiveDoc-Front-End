feab64d4c743db1fa73b425378a5dff992f80e30
aakarshan <aakartionsimkhada@gmail.com>
2024-08-22 20:41:34 -0700

    Button, collorator's avatar is added in share popup window. ower will be able to remove collaborators as well

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -16,6 +16,7 @@ import Image from "next/image";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import UserTypeSelector from "./UserTypeSelector";
+import Collaborator from "./Collaborator";
 
 const ShareModal = ({
   roomId,
@@ -66,6 +67,28 @@ const ShareModal = ({
             />
             <UserTypeSelector userType={userType} setUserType={setUserType} />
           </div>
+          <Button
+            type="submit"
+            onClick={shareDocumentHandler}
+            className="gradient-blue flex h-full gap-1 px-5"
+            disabled={loading}
+          >
+            {loading ? "Sending..." : "Invite"}
+          </Button>
+        </div>
+        <div className="my-2 space-y-2">
+          <ul className="flex flex-col">
+            {collaborators.map((collaborator) => (
+              <Collaborator
+                key={collaborator.id}
+                roomId={roomId}
+                creatorId={creatorId}
+                email={collaborator.email}
+                collaborator={collaborator}
+                user={user.info}
+              />
+            ))}
+          </ul>
         </div>
       </DialogContent>
     </Dialog>
